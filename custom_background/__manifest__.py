15cc756e3ca9dd8a3a964b597ce1bfe6cf38ca23
Christopher Ormaza <cormaza@opa-consulting.com>
2021-10-27 15:54:18 -0500

    IMP: agregado de varias utilidades por revisar en odoo.sh

---
+++ custom_background/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Custom Background",
+    "version": "14.0.0.0.10",
+    "author": "BizzAppDev",
+    "website": "http://www.bizzappdev.com",
+    "category": "GenericModules",
+    "depends": ["base", "web"],
+    "summary": "Custom  Background",
+    "images": ['images/image.png'],
+    "init_xml": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ir_actions.xml",
+        "views/res_company_view.xml",
+        "views/report_template.xml",
+    ],
+    "demo_xml": [],
+    "test": [],
+    "installable": True,
+    "application": False,
+    "license": "Other proprietary",
+}
